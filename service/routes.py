02aa1c35a309a39780859db7d58c00ebf19ce6b3
Gitesh Chinawalkar <gitesh1699@gmail.com>
2025-08-01 21:13:32 +0000

    linting test change

--- service/routes.py
+++ service/routes.py
@@ -23,7 +23,7 @@ and Delete Shopcart
 
 from flask import jsonify, request, url_for, abort
 from flask import current_app as app  # Import Flask application
-from flask_restx import Api, Resource, fields  # , reqparse, inputs
+from flask_restx import Api, Resource  # , fields  # , reqparse, inputs
 from service.models import Shopcart
 from service.common import status  # HTTP Status Codes
 
